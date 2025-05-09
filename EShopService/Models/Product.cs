bd21a4989822e930d6e75828b88eaf964f6a0c8e
TomekWojdyla <tw.kota@gmail.com>
2025-05-09 08:09:52 +0200

    Add project files.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿namespace EShopService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Ean { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public int Stock { get; set; } = 0;
+
+        public string Sku { get; set; } = string.Empty;
+
+        public Category Category { get; set; } = default!;
+    }
+}
