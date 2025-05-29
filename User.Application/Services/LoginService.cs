7e22d50cf8d960fc31afbaf0907e5bfd3ca1b9de
TomekWojdyla <tw.kota@gmail.com>
2025-05-29 20:40:31 +0200

    Lab 9 Ex 2

--- User.Application/Services/LoginService.cs
+++ User.Application/Services/LoginService.cs
@@ -11,10 +11,12 @@ namespace User.Application.Services;
 public class LoginService : ILoginService
 {
     protected IJwtTokenService _jwtTokenService;
+    protected Queue<int> _userLoggedIdsQueue;
 
-    public LoginService(IJwtTokenService jwtTokenService)
+    public LoginService(IJwtTokenService jwtTokenService, Queue<int> userLoggedIdsQueue )
     {
         _jwtTokenService = jwtTokenService;
+        _userLoggedIdsQueue = userLoggedIdsQueue;
     }
 
     public string Login(string username, string password)
@@ -23,18 +25,21 @@ public class LoginService : ILoginService
         {
             var roles = new List<string> { "Client", "Employee", "Administrator" };
             var token = _jwtTokenService.GenerateToken(123, roles);
+            _userLoggedIdsQueue.Enqueue(123);
             return token;
         }
         else if (username == "employee" && password == "haslo")
         {
             var roles = new List<string> { "Client", "Employee" };
             var token = _jwtTokenService.GenerateToken(456, roles);
+            _userLoggedIdsQueue.Enqueue(456);
             return token;
         }
         else if (username == "client" && password == "haslo")
         {
             var roles = new List<string> { "Client" };
             var token = _jwtTokenService.GenerateToken(789, roles);
+            _userLoggedIdsQueue.Enqueue(789);
             return token;
         }
         else
